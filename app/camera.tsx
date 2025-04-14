6ad164363eba59b072bac60395d5498d899671f1
Al-Bee <46373369+Al-Bee@users.noreply.github.com>
2025-04-14 22:26:28 +1200

    Fixed an oopsie :)

---
+++ app/camera.tsx
@@ -0,0 +1,99 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scannedData, setScannedData] = useState<string | null>(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to access the camera for barcode scanning</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function handleBarcodeScanned({ data }: { data: string }) {
+    setScannedData(data);
+    //alert(`Scanned barcode data: ${data}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} barcodeScannerSettings={{
+        barcodeTypes: ['ean13']}}
+      onBarcodeScanned={scannedData ? undefined : handleBarcodeScanned}>
+      </CameraView>
+      {scannedData && (
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>Scanned: {scannedData}</Text>
+        <Button title="Reset Scanner" onPress={() => setScannedData(null)} />
+      </View>
+    )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#336699'
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    width: '90%',
+    height: 300,
+    borderRadius: 16,
+    overflow: 'hidden',
+    alignSelf: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  resultContainer: {
+    position: 'absolute',
+    bottom: 40,
+    left: 20,
+    right: 20,
+    backgroundColor: '#99CCFF',
+    padding: 12,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  resultText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
