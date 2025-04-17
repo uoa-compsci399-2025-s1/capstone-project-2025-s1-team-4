692180a53fcd9c2f82adb4e5b3a4a4634df96c2e
Jaime Bahan <jaimebahan@gmail.com>
2025-04-17 12:16:25 +1200

    fix add_medicine script to use relative pathing

--- flask-server/add_medicine.py
+++ flask-server/add_medicine.py
@@ -1,8 +1,11 @@
 import sqlite3
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # flask-server/
+db_path = os.path.join(BASE_DIR, "medicine.db")
+
 def check_tables():
-    with sqlite3.connect(r"C:\programming\MD\capstone-project-2025-s1-team-4\flask-server\medicine.db") as con:
+    with sqlite3.connect(db_path) as con:
         cur = con.cursor()
         cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = cur.fetchall()
@@ -10,7 +13,7 @@ def check_tables():
 
 def add_medicine(name, company, dosage, cmi, barcode):
     data = [None, name, company, dosage, cmi, str(barcode)]
-    with sqlite3.connect(r"C:\programming\MD\capstone-project-2025-s1-team-4\flask-server\medicine.db") as con:
+    with sqlite3.connect(db_path) as con:
         cur = con.cursor()
         cur.execute("INSERT INTO medicine VALUES(?, ?, ?, ?, ?, ?)", data)
 
