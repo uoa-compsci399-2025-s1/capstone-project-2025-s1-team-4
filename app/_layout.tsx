55ee8acb5e3bee930ba0e80a239d88d532f71573
Al-Bee <46373369+Al-Bee@users.noreply.github.com>
2025-04-20 11:29:35 +1200

    Removed stack.screen for pages that are now tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,12 @@ export default function RootLayout() {
   return (
     <>
       {/* Global status bar config */}
-      <StatusBar style="auto" /> {/* or "light" if your default background is dark */}
+      <StatusBar style="auto" />
 
       <Stack>
-        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        {/* <Stack.Screen name="index" options={{ title: 'Home' }} />
         <Stack.Screen name="camera" options={{ title: 'Camera' }} />
-        <Stack.Screen name="scan_result" options={{ title: 'Scan results' }} />
+        <Stack.Screen name="scan_result" options={{ title: 'Scan results' }} /> */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </>
