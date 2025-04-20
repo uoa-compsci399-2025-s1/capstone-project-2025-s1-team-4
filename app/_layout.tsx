d2ad4eb63fc53549ecb50e80442a0c8e8e414f3b
Al-Bee <46373369+Al-Bee@users.noreply.github.com>
2025-04-20 12:09:10 +1200

    Switched to full tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,11 @@
-import { Stack, Tabs } from "expo-router";
-import Ionicons from '@expo/vector-icons'
+import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
-      {/* Global status bar config */}
       <StatusBar style="auto" />
-
-      <Stack>
-        {/* <Stack.Screen name="index" options={{ title: 'Home' }} />
-        <Stack.Screen name="camera" options={{ title: 'Camera' }} />
-        <Stack.Screen name="scan_result" options={{ title: 'Scan results' }} /> */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <Slot />
     </>
   );
 }
