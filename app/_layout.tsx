df849639c418e7568745ee561e1e812236208a7b
Al-Bee <46373369+Al-Bee@users.noreply.github.com>
2025-05-01 12:42:06 +1200

    Changed default search bar to dark

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <Slot />
     </>
   );
