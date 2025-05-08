07858f5f354826b23df51cba780aaeebc4d4b023
Jaime Bahan <jaimebahan@gmail.com>
2025-05-08 22:36:52 +1200

    further fixes including settings navigation

---
+++ app/(settings_pages)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+export default function SettingsLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        title: '',
+        headerShown: false,
+      }}
+    />
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,15 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <>
-      <Stack>
+      <Stack screenOptions={{
+        headerBackTitle: 'Back', // ← removes text next to back button everywhere
+        title: "",
+      }}>
         {/* Tabs as one screen */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false,  }} />
         {/* Stack-only screens */}
-        <Stack.Screen name="medicine_info" options={{ title: 'Medicine Info' }} />
+        <Stack.Screen name="medicine_info" options={{  }} />
+        <Stack.Screen name="(settings_pages)" options={{ headerShown: true, }} />
       </Stack>
       <StatusBar style="dark" />
       {/* <Slot /> */}
