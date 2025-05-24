afbe0691c69cddc2630ff9573c0fc7a76d0dac8f
Jaime Bahan <jaimebahan@gmail.com>
2025-05-25 00:03:29 +1200

    comment out sqlite database connection

--- flask-server/db_con.py
+++ flask-server/db_con.py
@@ -16,3 +16,17 @@ def get_repo():
     if 'repo' not in g:
         g.repo = MedicineRepo(get_db())
     return g.repo
+
+
+# To use the sqlite database:
+
+# def get_db():
+#     if 'db' not in g:
+#         g.db = sqlite3.connect("medicine.db")
+#         g.db.execute("PRAGMA foreign_keys = ON")
+#     return g.db
+
+# def get_repo():
+#     if 'repo' not in g:
+#         g.repo = MedicineRepo(get_db())
+#     return g.repo
