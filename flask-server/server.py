6548604d5ecebd7a426a7da775a0ca022455e227
Jaime Bahan <jaimebahan@gmail.com>
2025-05-25 00:09:55 +1200

    tidy up server file

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,4 @@
 from flask import Flask, g
-import sqlite3
-import IMedicineRepo as repo
-from MedicineRepo import MedicineRepo
-from db_con import get_db
-from init_db import create_tables
 from flask_cors import CORS
 
 
@@ -11,24 +6,12 @@ app = Flask(__name__)
 CORS(app)
 
 
-# Set up database connection
-
-# connection=sqlite3.connect('medicine.db')
-# cursor=connection.cursor()
-# create_tables(cursor) ## Function to check if tables already exist, if not then create
-# connection.execute("PRAGMA foreign_keys = ON") # enables foreign keys
-# connection.commit()
-
-
+# Close db connection after every requests
 @app.teardown_appcontext
 def close_db(error):
     db = g.pop('db', None)
     if db is not None:
         db.close()
-
-
-# Data repository instance
-# repo.repo_instance = MedicineRepo(connection)
 
 
 # Register blueprints (api routes)
